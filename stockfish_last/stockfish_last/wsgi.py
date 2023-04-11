ddcd5748cdfb99faa17e6f9d7be49d05ac94beab
emik61 <kozemirhan98@gmail.com>
2023-04-11 15:51:07 +0300

    deneme2

--- stockfish_last/stockfish_last/wsgi.py
+++ stockfish_last/stockfish_last/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockfish_last.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockfish_last.settings_prodction')
 
 application = get_wsgi_application()
