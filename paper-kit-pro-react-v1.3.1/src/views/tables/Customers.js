bf2daaac987b5c6cd8c3359b0b145119fe9a6978
kozomer <kozomer@gmail.com>
2023-04-14 23:21:27 +0300

    customer urls

--- paper-kit-pro-react-v1.3.1/src/views/tables/Customers.js
+++ paper-kit-pro-react-v1.3.1/src/views/tables/Customers.js
@@ -45,7 +45,7 @@ const DataTable = () => {
     async function fetchData() {
       const access_token = await localforage.getItem('access_token'); 
       console.log(access_token)
-      const response = await fetch('http://127.0.0.1:8000/customers/',{
+      const response = await fetch('https://vividstockfish.com/api/customers/',{
        
         headers: {
           'Content-Type': 'application/json',
@@ -83,7 +83,7 @@ const DataTable = () => {
     const formData = new FormData();
     formData.append('file', file);
     const access_token = await localforage.getItem('access_token');
-    fetch('http://127.0.0.1:8000/add_customers/', {
+    fetch('https://vividstockfish.com/api/add_customers/', {
       method: 'POST',
       body: formData,
       headers: {
@@ -104,7 +104,7 @@ const DataTable = () => {
         setIsLoading(false);
         successUpload(data.message);
         
-        fetch('http://127.0.0.1:8000/customers/',{
+        fetch('https://vividstockfish.com/api/customers/',{
           headers: {
             'Authorization': 'Bearer '+ String(access_token)
           }
@@ -246,7 +246,7 @@ const DataTable = () => {
       if (deleteConfirm) {
        console.log("delete")
        const access_token =  await localforage.getItem('access_token'); 
-        fetch(`http://127.0.0.1:8000/delete_customers/`, {
+        fetch(`https://vividstockfish.com/api/delete_customers/`, {
           method: "POST",
           body: new URLSearchParams(deleteData),
           headers: {
@@ -305,7 +305,7 @@ const DataTable = () => {
         
       };
       console.log(updatedData)
-      fetch('http://127.0.0.1:8000/edit_customers/', {
+      fetch('https://vividstockfish.com/api/edit_customers/', {
       method: 'POST',
       body: JSON.stringify(updatedData),
       headers: {
@@ -346,7 +346,7 @@ const DataTable = () => {
       const access_token = await localforage.getItem('access_token');
     
       // Make an AJAX request to the backend to download the CSV file
-      const response = await fetch('http://127.0.0.1:8000/export_customers/', {
+      const response = await fetch('https://vividstockfish.com/api/export_customers/', {
         headers: {
           'Authorization': 'Bearer '+ String(access_token)
         },
