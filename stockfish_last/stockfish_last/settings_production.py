ba597869bc183431336d7c31940658247d242555
kozomer <kozomer@gmail.com>
2023-04-14 12:45:40 +0300

    1

--- stockfish_last/stockfish_last/settings_production.py
+++ stockfish_last/stockfish_last/settings_production.py
@@ -113,7 +113,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'stockfish_last.urls'
